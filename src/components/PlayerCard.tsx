94d04f45325df0dce131300374412279f6b237fa
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-25 02:11:10 +0500

    show player data in player card

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -4,7 +4,9 @@ type Props = {
     player: Player;
 }
 export function PlayerCard({ player }: Props) {
-    return <div id="player-card" className="w-60 border">
-        Player Card
+    return <div id="player-card" className="w-60 border p-3">
+        <p>{player.realName}</p>
+        <p>{player.playerName}</p>
+        <p>{player.asset}</p>
     </div>
 }
