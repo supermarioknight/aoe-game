d42f4435327d2bfb10cf2b8b7f6a7b8ef7893673
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-25 02:11:41 +0500

    make player card rounded and add shadow

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -4,7 +4,7 @@ type Props = {
     player: Player;
 }
 export function PlayerCard({ player }: Props) {
-    return <div id="player-card" className="w-60 border p-3">
+    return <div id="player-card" className="w-60 border p-3 rounded-md shadow-md">
         <p>{player.realName}</p>
         <p>{player.playerName}</p>
         <p>{player.asset}</p>
