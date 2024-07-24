80dfdad674dd2b00c09758f78aeb55d0badda401
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-25 02:20:25 +0500

    accept isactive parameter in props to show if the selected player is same as player cardd

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -3,9 +3,10 @@ import { Player } from "../interfaces/Player.interface"
 
 type Props = {
     player: Player;
+    isActive?: boolean;
 }
-export function PlayerCard({ player }: Props) {
-    return <div id="player-card" className="w-40 border p-3 rounded-md shadow-md hover:cursor-pointer hover:bg-gray-100">
+export function PlayerCard({ player, isActive }: Props) {
+    return <div id="player-card" className={"w-40 border p-3 rounded-md shadow-md hover:cursor-pointer hover:bg-gray-100"}>
         <Paragraph
             ellipsis={{
                 rows: 1,
