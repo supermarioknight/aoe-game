a736c2bfdaa3b2f72c81c75caa4bdea152ae5fd0
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-25 02:32:30 +0500

    bind methods to controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -5,19 +5,19 @@ type Props = {
     onSubmit: () => void;
 }
 
-export function Controls() {
+export function Controls({ onSort, onSubmit }: Props) {
 
     return <Card className="!shadow-md" title="Controls" style={{ minHeight: 300 }}>
         <div id="controls" className="h-full gap-2 flex flex-col items-center justify-center">
             <div id="controls-btns" className="flex items-center space-x-2">
-                <Button>
+                <Button onClick={() => onSort('asc')}>
                     Sort Asc
                 </Button>
-                <Button>
+                <Button onClick={() => onSort('desc')}>
                     Sort Desc
                 </Button>
             </div>
-            <Button type="primary">
+            <Button type="primary" onClick={onSubmit}>
                 Submit
             </Button>
         </div>
