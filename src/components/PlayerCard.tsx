31e3b6c85b85052e81f9cc258cf1836905fc8315
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-25 02:17:41 +0500

    add ellipsis to paragraphs

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,12 +1,27 @@
+import Paragraph from "antd/es/typography/Paragraph";
 import { Player } from "../interfaces/Player.interface"
 
 type Props = {
     player: Player;
 }
 export function PlayerCard({ player }: Props) {
-    return <div id="player-card" className="w-60 border p-3 rounded-md shadow-md">
-        <li>{player.realName}</li>
-        <li>{player.playerName}</li>
-        <li>{player.asset}</li>
+    return <div id="player-card" className="w-32 border p-3 rounded-md shadow-md">
+        <Paragraph
+            ellipsis={{
+                rows: 1,
+                expandable: false,
+            }}
+        >{player.realName}</Paragraph>
+        <Paragraph
+            ellipsis={{
+                rows: 1,
+                expandable: false,
+            }}
+        >{player.playerName}</Paragraph>
+        <Paragraph
+            ellipsis={{
+                rows: 1,
+                expandable: false,
+            }}>{player.asset}</Paragraph>
     </div>
 }
