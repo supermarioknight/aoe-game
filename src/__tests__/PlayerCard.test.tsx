3ec77522bc17590f3742fa2b8846efb0b0e4b16e
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-26 03:27:55 +0500

    test applies active class when isActive is true

---
+++ src/__tests__/PlayerCard.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Player } from '../interfaces/Player.interface';
+import { describe, expect, test } from 'vitest';
+import { PlayerCard } from '../components/PlayerCard';
+import "@testing-library/jest-dom/vitest"
+
+
+
+const mockPlayer: Player = {
+    realName: 'John Doe',
+    playerName: 'john_doe',
+    asset: 'Sword',
+};
+
+
+describe('PlayerCard Component', () => {
+
+    test('displays player details', () => {
+        render(<PlayerCard player={mockPlayer} />);
+        expect(screen.getByText('John Doe')).toBeInTheDocument();
+        expect(screen.getByText('john_doe')).toBeInTheDocument();
+        expect(screen.getByText('Sword')).toBeInTheDocument();
+    });
+
+    test('applies active class when isActive is true', () => {
+        const { container } = render(<PlayerCard player={mockPlayer} isActive={true} />);
+        expect(container.firstChild).toHaveClass('bg-gray-100');
+    });
+
+})
