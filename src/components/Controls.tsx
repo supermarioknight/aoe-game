2124d76828ecd23429e3576370d6f06d014d1306
supermarioknight <zadeikisdonatas@gmail.com>
2024-07-26 16:51:40 -0400

    tweak styling

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -9,17 +9,17 @@ type Props = {
 export function Controls({ onSort, onSubmit, order }: Props) {
   return (
     <Card className="!shadow-md" title="Controls" style={{ minHeight: 300 }}>
-      <div id="controls" className="h-full gap-2 flex flex-col items-center justify-center">
-        <div id="controls-btns" className="flex items-center space-x-2">
+      <div id="controls" className="h-full gap-2 flex flex-col items-center justify-center w-52 mx-auto">
+        <div id="controls-btns" className="flex items-center justify-between w-full mb-4">
           <Button type={order === 'asc' ? 'primary' : undefined} onClick={() => onSort('asc')}>
-            Sort Asc
+            SORT ASC
           </Button>
           <Button type={order === 'desc' ? 'primary' : undefined} onClick={() => onSort('desc')}>
-            Sort Desc
+            SORT DESC
           </Button>
         </div>
-        <Button type="primary" onClick={onSubmit}>
-          Submit
+        <Button type="primary" onClick={onSubmit} block>
+          SUBMIT
         </Button>
       </div>
     </Card>
